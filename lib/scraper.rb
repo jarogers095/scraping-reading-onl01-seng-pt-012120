3c59ceb96a09825b5bb0e0276f8c8a66c7c963a1
James Rogers <jarogers095@gmail.com>
2020-02-21 04:40:16 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -13,3 +13,4 @@ end
 #document.querySelector("#cd016a4c-d090-5b88-9be3-63e6d9187edf > div > div > div > div:nth-child(2) > div.text-4GLMvr > div.title-oE5vT4")
 
 
+#\32 3e80838-4c6a-5ee6-827a-76ac92772145 > div > div > div > div:nth-child(2) > div.text-4GLMvr > div.title-oE5vT4
