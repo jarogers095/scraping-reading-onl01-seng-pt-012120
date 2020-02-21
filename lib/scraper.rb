7b5bd79d020bd403cd189bc784e51563c32191ef
James Rogers <jarogers095@gmail.com>
2020-02-21 04:58:23 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,7 @@ require 'open-uri'
 html = open("https://flatironschool.com/our-courses")
 doc = Nokogiri::HTML(html)
 
-courses = doc.css(".text-4GLMvr.title-oE5vT4")
+courses = doc.css(".text-4GLMvr .title-oE5vT4")
 
 courses.each do |course|
   puts course.text
