483a87966a549da687ccb4d5c6ff5a89d0498bcd
James Rogers <jarogers095@gmail.com>
2020-02-21 04:52:50 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,7 +1,7 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+html = open("https://flatironschool.com/our-courses")
 doc = Nokogiri::HTML(html)
 
 courses = doc.css(".inlineMobileLeft-2Yo002 div.text-4GLMvr div.title-oE5vT4")
