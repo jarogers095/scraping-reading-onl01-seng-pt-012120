912ce21b183e265c71769a6997be561464fb4dbe
James Rogers <jarogers095@gmail.com>
2020-02-21 04:46:35 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,7 @@ doc = Nokogiri::HTML(html)
 courses = doc.css("#cd016a4c-d090-5b88-9be3-63e6d9187edf div div div div:nth-child(2) .text-4GLMvr .title-oE5vT4")
 
 courses.each do |course|
-  puts course
+  puts course.text
 end
 
 #cd016a4c-d090-5b88-9be3-63e6d9187edf > div > div > div > div:nth-child(2) > div.text-4GLMvr > div.title-oE5vT4
