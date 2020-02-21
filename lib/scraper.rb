527cdc267e339bd9df254e8ea60421a4f05cc4c3
James Rogers <jarogers095@gmail.com>
2020-02-21 04:39:48 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,14 +4,12 @@ require 'open-uri'
 html = open("https://flatironschool.com/")
 doc = Nokogiri::HTML(html)
 
-courses = doc.css(".title-oE5vT4")
+courses = doc.css("div.title-oE5vT4")
 
 courses.each do |course|
   puts course.text.strip
 end
 
-document.querySelector("#cd016a4c-d090-5b88-9be3-63e6d9187edf > div > div > div > div:nth-child(2) > div.text-4GLMvr > div.title-oE5vT4")
+#document.querySelector("#cd016a4c-d090-5b88-9be3-63e6d9187edf > div > div > div > div:nth-child(2) > div.text-4GLMvr > div.title-oE5vT4")
 
-#cd016a4c-d090-5b88-9be3-63e6d9187edf > div > div > div > div:nth-child(2) > div.text-4GLMvr > div.title-oE5vT4
 
-//*[@id="cd016a4c-d090-5b88-9be3-63e6d9187edf"]/div/div/div/div[2]/div[2]/div[1]
