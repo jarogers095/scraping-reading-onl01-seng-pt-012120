12e22c9e58e03349d464c31531598be664269f8c
James Rogers <jarogers095@gmail.com>
2020-02-21 04:48:52 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -15,4 +15,4 @@ end
 
 #\32 3e80838-4c6a-5ee6-827a-76ac92772145 > div > div > div > div:nth-child(2) > div.text-4GLMvr > div.title-oE5vT4
 
-//*[@id="cd016a4c-d090-5b88-9be3-63e6d9187edf"]/div/div/div/div[2]/div[2]/div[1]
+#//*[@id="cd016a4c-d090-5b88-9be3-63e6d9187edf"]/div/div/div/div[2]/div[2]/div[1]
